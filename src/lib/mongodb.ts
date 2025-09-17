980a0a8a55f108d45d430975b14ae394ac76c86b
Omar Fornaroli <OmarFornaroli@gmail.com>
2025-09-17 23:20:02 +0000

    falla npm run build 43.84 Error: Please define the MONGO_URI environment

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = process.env.MONGO_URI;
-
-if (!MONGO_URI) {
-  throw new Error('Please define the MONGO_URI environment variable inside .env.local');
-}
-
 let cachedClient: typeof mongoose | null = null;
 
 export async function connectToDatabase() {
   if (cachedClient) {
     return cachedClient;
+  }
+  
+  const MONGO_URI = process.env.MONGO_URI;
+
+  if (!MONGO_URI) {
+    throw new Error('Please define the MONGO_URI environment variable inside .env or pass it at runtime');
   }
 
   try {
